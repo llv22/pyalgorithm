9bdf1cbdbc95bd26b540aa79b4de19614d109a8c
Orlando Ding <i058959@cnpvgl903653a.apj.global.corp.sap>
2021-07-21 13:11:35 +0800

    orlando - for adding changes of pyalgorithm

--- 01_dp/Longest_Palindromic_Substring.py
+++ 01_dp/Longest_Palindromic_Substring.py
@@ -6,21 +6,11 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.3'
-#       jupytext_version: 0.8.2
+#       jupytext_version: 0.8.6
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
-#   language_info:
-#     codemirror_mode:
-#       name: ipython
-#       version: 3
-#     file_extension: .py
-#     mimetype: text/x-python
-#     name: python
-#     nbconvert_exporter: python
-#     pygments_lexer: ipython3
-#     version: 3.6.7
 # ---
 
 # # 5. Longest Palindromic Substring
