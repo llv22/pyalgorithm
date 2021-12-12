5e49dd566fc95c944b7756def9305ed4da217db8
Orlando, Ding <or.ding@sap.com>
2021-12-12 13:34:39 +0800

    orlando - for adding new implementation

---
+++ 986.interval-list-intersections.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=986 lang=python3
+#
+# [986] Interval List Intersections
+#
+
+# @lc code=start
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        
+# @lc code=end
+
