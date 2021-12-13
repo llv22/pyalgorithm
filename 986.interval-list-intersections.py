98e464a740ea8f4d5b13dfa3616bf68208aabf22
Orlando, Ding <or.ding@sap.com>
2021-12-13 18:19:04 +0800

    orlando - for udpates of codes

--- 986.interval-list-intersections.py
+++ 986.interval-list-intersections.py
@@ -10,6 +10,15 @@ from typing import List
 
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        pass
+        i = j = 0; output = []
+        while i < len(firstList) and j < len(secondList):
+            start = max(firstList[i][0], secondList[j][0])
+            if firstList[i][1] <= secondList[j][1]:
+                end = firstList[i][1]; i += 1
+            else:
+                end = secondList[j][1]; j += 1
+            if start <= end:
+                output.append([start, end])
+        return output
 
 # @lc code=end
