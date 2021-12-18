c3d9e037d8c8e9e9efe5c90a8bbd70e7610ba1f5
Orlando Ding <xiandao.airs@gmail.com>
2021-12-18 10:21:55 +0800

    orlando - for updates of today's assignment

---
+++ 4.median-of-two-sorted-arrays.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=4 lang=python3
+#
+# [4] Median of Two Sorted Arrays
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        pass
+
+# @lc code=end
+
