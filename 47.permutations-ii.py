800c14466e0ad02dead7d96f8b3ec5f948949911
Orlando, Ding <or.ding@sap.com>
2022-01-05 12:01:16 +0800

    orlando - for updates of items

--- 47.permutations-ii.py
+++ 47.permutations-ii.py
@@ -8,7 +8,24 @@
 from typing import List
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        pass
+        def dfs(nums: List[int], cur: int):
+            nonlocal result
+            if cur == len(nums):
+                result.append(nums.copy())
+                return
+            for i in range(cur, len(nums)):
+                nums[cur], nums[i] = nums[i], nums[cur]
+                if nums[:cur+1] not in [l[:cur+1] for l in result]:
+                    dfs(nums, cur+1)
+                nums[cur], nums[i] = nums[i], nums[cur]
+        
+        result = []
+        dfs(nums, 0)
+        return result
         
 # @lc code=end
 
+if __name__ == '__main__':
+    proxy = Solution()
+    proxy.permuteUnique([1,1,2])
+    
